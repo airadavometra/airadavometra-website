ec9d33a541a9c0594900e4715cb2693effc69ada
airadavometra <airadavometra@gmail.com>
2021-05-23 20:44:33 +0300

    change svg, make links and alignings

--- src/components/GridItemVertical/GridItemVertical.tsx
+++ src/components/GridItemVertical/GridItemVertical.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import classes from './GridItemVertical.module.scss';
 
 export interface GridItemVerticalProps {
