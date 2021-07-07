3d6c5b3ab210113cb9e503c078dac97e1806ca14
Daria Artemova <airadavometra@gmail.com>
2021-07-07 21:47:23 +0300

    add new video

--- src/pages/VideoPage/VideoPage.tsx
+++ src/pages/VideoPage/VideoPage.tsx
@@ -5,6 +5,7 @@ import classes from './VideoPage.module.scss';
 export const VideoPage: FunctionComponent = () => {
   return (
     <div className={classes.main}>
+      <VideoItem src="https://www.youtube.com/embed/O1P0SegrR8w" />
       <VideoItem src="https://www.youtube.com/embed/wKEOvMF5R-c" />
       <VideoItem src="https://www.youtube.com/embed/x7HXwgfPjIg" />
       <VideoItem src="https://www.youtube.com/embed/qDYJ6ewVQqY" />
